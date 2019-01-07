a408880294062ee934025b22fa9985e5c6bfc296
Liam <LiamFisher1@gmail.com>
2019-01-07 13:24:43 +0000

    Made PathBase configurable

--- Startup.cs
+++ Startup.cs
@@ -34,10 +34,43 @@ namespace KubernetesSubpathExample
             });
         }
 
+        /// <summary>
+        /// Gets PathBase from configuration
+        /// </summary>
+        /// <returns>PathBase from configuration or string.Empty if not configured.</returns>
+        string GetPathBase()
+        {
+            var pathBase = Configuration["PathBase"];
+
+            if (string.IsNullOrEmpty(pathBase))
+            {
+                return string.Empty;
+            }
+
+            // Avoid double forward slash
+            if (pathBase.Trim() == "/")
+            {
+                return string.Empty;
+            }
+
+            // If the path is specified but doesn't begin with a forward slash
+            // throw an exception. Root path must begin with forward slash.
+            // Better to explicitly require it than make it magic.
+            if (pathBase.StartsWith("/") == false 
+                && string.IsNullOrEmpty(pathBase) == false)
+            {
+                throw new Exception("If Configuration[\"PathBase\"] is specified it must begin with /");
+            }
+
+            return pathBase;
+        }
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            var pathBase = "/helloworld";
+            // Get pathbase from configuration, default to string.Empty
+            var pathBase = GetPathBase();
+
             // Change the Path Base
             app.UsePathBase(pathBase);
             app.UseStaticFiles(pathBase);
